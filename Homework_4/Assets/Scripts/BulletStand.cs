f7ceac311ccee69e67e2f499793dd99d2cff03b9
Igor Sokol <igor.sokol.isa@gmail.com>
2022-09-16 15:40:00 +0300

    Rename bullet stand

--- Homework_4/Assets/Scripts/BulletStand.cs
+++ Homework_4/Assets/Scripts/BulletStand.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Stand : MonoBehaviour
+public class BulletStand : MonoBehaviour
 {
     [SerializeField] private Bullet bulletPrefab;
 
